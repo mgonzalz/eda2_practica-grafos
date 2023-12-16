6c6023f616b5ac5dfd4c6dfa50761be2602756dd
luciacantos <luciacantos22@gmail.com>
2023-12-16 18:02:45 +0100

    Clase Ciudad

---
+++ modulos/classGrafo.py
@@ -0,0 +1,8 @@
+class Ciudad(object):
+    def __init__(self,nombre):
+        self.nombre = nombre
+        self.conexiones = {}
+
+    def agregar_conexion(self, cuidad, peso):
+        self.conexiones[ciudad] = peso
+        
