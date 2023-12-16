2780be769ccf9066df9e366bba2c32664461e66a
luciacantos <luciacantos22@gmail.com>
2023-12-16 18:28:04 +0100

    definicion de conexiones con sus pesos

--- modulos/classGrafo.py
+++ modulos/classGrafo.py
@@ -3,7 +3,7 @@ class Ciudad(object):
         self.nombre = nombre
         self.conexiones = {}
 
-    def agregar_conexion(self, cuidad, peso):
+    def agregar_conexion(self, ciudad, peso):
         self.conexiones[ciudad] = peso
 
 class Grafo(object):
@@ -20,4 +20,28 @@ class Grafo(object):
         for nombre, ciudad in sorted(self.ciudades.items()):
             print(f"{nombre} -> {ciudad.conexiones}")
 
-            
+if __name__ == '__main__':
+    g = Grafo()
+
+    for letra in map(chr, range(65,70)):
+        g.agregar_ciudad(Ciudad(letra))
+
+    conexiones_pesos = [
+        ('E', 'C', 2),
+        ('E', 'D', 3),
+        ('C', 'A', 1),
+        ('C', 'E', 4),
+        ('D', 'A', 2),
+        ('D', 'B', 1),
+        ('D', 'E', 3),
+        ('A', 'B', 5),
+        ('A', 'C', 2),
+        ('A', 'D', 4),
+        ('B', 'A', 1),
+        ('B', 'D', 3)
+    ]
+
+    for conexion in conexiones_pesos:
+        g.agregar_conexion(*conexion)
+
+    g.imprimir_grafo()
