66d0402782b4c80c42513ba3cec74f4dd4ccb99c
luciacantos <luciacantos22@gmail.com>
2023-12-16 18:20:42 +0100

    Clase Grafo

--- modulos/classGrafo.py
+++ modulos/classGrafo.py
@@ -5,4 +5,19 @@ class Ciudad(object):
 
     def agregar_conexion(self, cuidad, peso):
         self.conexiones[ciudad] = peso
-        
+
+class Grafo(object):
+    ciudades = {}
+
+    def agregar_ciudad(self, ciudad):
+        self.ciudades[ciudad.nombre] = ciudad
+
+    def agregar_conexion(self, ciudad1, ciudad2, peso):
+        self.ciudades[ciudad1].agregar_conexion(ciudad2, peso)
+        self.ciudades[ciudad2].agregar_conexion(ciudad1, peso)
+
+    def imprimir_grafo(self):
+        for nombre, ciudad in sorted(self.ciudades.items()):
+            print(f"{nombre} -> {ciudad.conexiones}")
+
+            
